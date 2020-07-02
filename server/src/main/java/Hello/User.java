17916c9ac0ddd38a528e96c22a16685b4887bfc3
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-02 15:22:11 +0000

    Base user class

---
+++ server/src/main/java/Hello/User.java
@@ -0,0 +1,37 @@
+package Hello;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
