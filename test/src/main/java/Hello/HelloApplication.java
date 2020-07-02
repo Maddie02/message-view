ec4402fc336de93e56f4a7dd9df589ef3db83f5c
yanica109 <yanica.georgieva109@gmail.com>
2020-07-02 12:53:41 +0300

    spring attempt

--- test/src/main/java/Hello/HelloApplication.java
+++ test/src/main/java/Hello/HelloApplication.java
@@ -1,13 +1,13 @@
-package TestHello;
+package Hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestHelloApplication {
+public class HelloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestHelloApplication.class, args);
+		SpringApplication.run(HelloApplication.class, args);
 	}
 
 }
--- test/src/test/java/TestHello/TestHelloApplicationTests.java
+++ test/src/test/java/TestHello/TestHelloApplicationTests.java
@@ -1,10 +1,10 @@
-package TestHello;
+package Hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestHelloApplicationTests {
+class HelloApplicationTests {
 
 	@Test
 	void contextLoads() {
