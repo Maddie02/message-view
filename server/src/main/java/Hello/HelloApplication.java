f784376a7d13f71b1e9beb9a785b18b2f1daaa47
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-02 15:30:56 +0000

    Update HelloApplication.java with new GetMapping

--- server/src/main/java/Hello/HelloApplication.java
+++ server/src/main/java/Hello/HelloApplication.java
@@ -18,4 +18,11 @@ public class HelloApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	@GetMapping("/register")
+	public String register() {
+		return "Register???";
+	}
+
+
 }
