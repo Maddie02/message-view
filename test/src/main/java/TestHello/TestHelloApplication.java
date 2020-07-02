d1af1c065c7eeacff7ace0e4492a021719bacc21
yanica109 <yanica.georgieva109@gmail.com>
2020-07-02 12:35:33 +0300

    spring attempt

---
+++ test/src/main/java/TestHello/TestHelloApplication.java
@@ -0,0 +1,13 @@
+package TestHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHelloApplication.class, args);
+	}
+
+}
---
+++ test/src/test/java/TestHello/TestHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package TestHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
