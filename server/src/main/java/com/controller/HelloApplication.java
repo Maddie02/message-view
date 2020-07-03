5b8577637910b0d12d525379aea781fae94181f2
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-03 09:56:36 +0000

    Upload New File

---
+++ server/src/main/java/com/controller/HelloApplication.java
@@ -0,0 +1,31 @@
+package com.conroller;
+
+import com.model.User;
+import com.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloApplication {
+
+	@Autowired
+	private UserRepo repository;
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping(value = "/getUsers", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<User>> register() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+
+}
