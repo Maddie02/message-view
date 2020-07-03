081c5c58da62080db61ada80c161d76efd888fec
Madlen <madlensarkisian@gmail.com>
2020-07-03 10:52:21 +0300

    Added response entity in return

--- server/src/main/java/com/server/controller/MessageController.java
+++ server/src/main/java/com/server/controller/MessageController.java
@@ -1,11 +1,14 @@
 package com.server.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.server.model.Message;
 import com.server.repository.MessageRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +18,19 @@ public class MessageController {
     @Autowired
     private MessageRepository messageRepository;
 
+    // @GetMapping("/getMessages")
+    // public List<Message> getMessages() {
+    //     return messageRepository.findAll();
+    // }
     @GetMapping("/getMessages")
-    public List<Message> getMessages() {
-        return messageRepository.findAll();
+    public ResponseEntity<List<Message>> getMessages() {
+        List<Message> messages = new ArrayList<Message>();
+        messageRepository.findAll().forEach(messages::add);
+
+        if (messages.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 }
