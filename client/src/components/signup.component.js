f98379a0c9efe88cfad19ffdab70305593770d0d
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-06 11:35:02 +0000

    Update signup.component.js with names for fields

--- client/src/components/signup.component.js
+++ client/src/components/signup.component.js
@@ -3,16 +3,16 @@ import React, { Component } from "react";
 export default class SignUp extends Component {
     render() {
         return (
-            <form>
+            <form action="http://localhost:8080/register" method="POST">
                 <h3>Sign Up</h3>
                 <div className="form-group">
                     <label>Username</label>
-                    <input type="text" className="form-control" placeholder="Enter username" />
+                    <input type="text" name="username" className="form-control" placeholder="Enter username" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" name="password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
