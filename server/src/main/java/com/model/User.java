b85cfadbec70881c91ed16f5375948bd4cd5b042
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-06 08:20:48 +0000

    Update User.java with encryption for password

--- server/src/main/java/com/model/User.java
+++ server/src/main/java/com/model/User.java
@@ -4,6 +4,10 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
 @Document(collection = "user")
 public class User {
     @Id
@@ -13,7 +17,27 @@ public class User {
 
     public User(String username, String password) {
         this.username = username;
-        this.password = password;
+
+        String encryptedPass = "";
+        String key = "PaSsEnCrIpTiOn!?";
+
+        try {
+            Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+
+            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            byte[] encrypted = cipher.doFinal(password.getBytes());
+            encryptedPass = new String(encrypted);
+            //System.err.println(encryptedPass);
+
+			/*cipher.init(Cipher.DECRYPT_MODE, aesKey);
+			String decrypted = new String(cipher.doFinal(encrypted));
+			System.err.println(decrypted);*/
+        } catch (Exception ex) {
+            System.out.println("Something went wrong!");
+        }
+
+        this.password = encryptedPass;
     }
 
     public ObjectId getId() {
@@ -28,3 +52,7 @@ public class User {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+}
