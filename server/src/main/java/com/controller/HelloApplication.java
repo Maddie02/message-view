c737eee46d2eaea8a8689a8018f8fd82ac9e857b
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-06 08:17:32 +0000

    Optimization

--- server/src/main/java/com/controller/HelloApplication.java
+++ server/src/main/java/com/controller/HelloApplication.java
@@ -7,15 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
 import java.util.List;
 
 @RestController
 public class HelloApplication {
-
-	private static final String key = "PaSsEnCrIpTiOn!?";
 
 	@Autowired
 	private UserRepo repository;
@@ -32,45 +27,12 @@ public class HelloApplication {
 
 	@GetMapping(value = "/createUser")
 	public void createUser(@RequestParam(value = "name", defaultValue = "Gosho") String name, @RequestParam(value = "pass", defaultValue = "Tosho") String pass) {
-
-		String encryptedPass = "";
-
-		try {
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-
-			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-			byte[] encrypted = cipher.doFinal(pass.getBytes());
-			encryptedPass = new String(encrypted);
-			//System.err.println(encryptedPass);
-
-			/*cipher.init(Cipher.DECRYPT_MODE, aesKey);
-			String decrypted = new String(cipher.doFinal(encrypted));
-			System.err.println(decrypted);*/
-		} catch (Exception ex) {
-			System.out.println("Something went wrong!");
-		}
-
-		repository.insert(new User(name, encryptedPass));
+		repository.insert(new User(name, pass));
 	}
 
 	@RequestMapping(value="/register", method = RequestMethod.POST)
 	public boolean register(@RequestParam("username") String username, @RequestParam("password") String password) {
-
-		String encryptedPass = "";
-
-		try {
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-			byte[] encrypted = cipher.doFinal(password.getBytes());
-			encryptedPass = new String(encrypted);
-		} catch (Exception ex) {
-			System.out.println("Something went wrong!");
-			return false;
-		}
-
-		repository.insert(new User(username, encryptedPass));
+		repository.insert(new User(username, password));
 		return true;
 	}
 
@@ -78,4 +40,4 @@ public class HelloApplication {
 	public String register() {
 		return "register???";
 	}
-}}
+}
