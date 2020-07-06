a36b1fca2971d83522dfb4847d65e92feb0088d7
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-06 07:44:12 +0000

    Update HelloApplication.java with hashing passwords

--- server/src/main/java/com/controller/HelloApplication.java
+++ server/src/main/java/com/controller/HelloApplication.java
@@ -7,10 +7,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 import java.util.List;
 
 @RestController
 public class HelloApplication {
+
+	private static final String key = "PaSsEnCrIpTiOn!?";
 
 	@Autowired
 	private UserRepo repository;
@@ -26,13 +31,46 @@ public class HelloApplication {
 	}
 
 	@GetMapping(value = "/createUser")
-	public void createUser(@RequestParam(value = "name", defaultValue = "Gosho") String name, @RequestParam(value = "pass", defaultValue = "Tosho") String pass){
-		repository.insert(new User(name, pass));
+	public void createUser(@RequestParam(value = "name", defaultValue = "Gosho") String name, @RequestParam(value = "pass", defaultValue = "Tosho") String pass) {
+
+		String encryptedPass = "";
+
+		try {
+			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			byte[] encrypted = cipher.doFinal(pass.getBytes());
+			encryptedPass = new String(encrypted);
+			//System.err.println(encryptedPass);
+
+			/*cipher.init(Cipher.DECRYPT_MODE, aesKey);
+			String decrypted = new String(cipher.doFinal(encrypted));
+			System.err.println(decrypted);*/
+		} catch (Exception ex) {
+			System.out.println("Something went wrong!");
+		}
+
+		repository.insert(new User(name, encryptedPass));
 	}
 
 	@RequestMapping(value="/register", method = RequestMethod.POST)
 	public boolean register(@RequestParam("username") String username, @RequestParam("password") String password) {
-		repository.insert(new User(username, password));
+
+		String encryptedPass = "";
+
+		try {
+			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			byte[] encrypted = cipher.doFinal(password.getBytes());
+			encryptedPass = new String(encrypted);
+		} catch (Exception ex) {
+			System.out.println("Something went wrong!");
+			return false;
+		}
+
+		repository.insert(new User(username, encryptedPass));
 		return true;
 	}
 
@@ -40,6 +78,4 @@ public class HelloApplication {
 	public String register() {
 		return "register???";
 	}
-
-
-}
+}}
