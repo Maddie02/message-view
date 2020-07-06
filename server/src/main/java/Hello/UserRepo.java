8b7ea9cf936d309ce2c1c3682efaa81eff632b65
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-06 06:54:28 +0000

    Delete UserRepo.java

--- server/src/main/java/Hello/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package Hello;
-
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepo extends MongoRepository<User, String> {
-    public User findByUsername(String username);
-}
