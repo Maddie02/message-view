8791bcd3acd7725a360b065b12cb43419df08832
Madlen <madlensarkisian@gmail.com>
2020-07-06 15:14:34 +0300

    [New] Added message listing and refactored some components

---
+++ client/src/components/Messages.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import axios from 'axios';
+import './Messages.css';
+
+const Message = props => {
+    return (
+        <Grid item component={Card} xs={12} md={3} sm={6} className="card">
+            <CardContent>
+                <Typography color="textSecondary">
+                    {props.message.id}
+                </Typography>
+                <Typography variant="h5">
+                    {props.message.text}
+                </Typography>
+            </CardContent>
+        </Grid>
+    );
+}
+
+const Messages = () => {
+
+    const [messages, setMessages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get('http://localhost:8080/getMessages')
+             .then(response => {
+                 setMessages(response.data);
+                 setIsLoading(false);
+                })
+             .catch(error => console.log(error));
+    }, [])
+
+    const exerciseList = () => {
+        return messages.map(currentMessage => {
+            return <Message message={currentMessage} key={currentMessage.id} />
+        })
+    }
+
+    return (
+        <div className="cards">
+            <br />
+            <div className="container">
+                {isLoading ? 
+                <div>
+                    <img className="loader" src={'https://media2.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif'} alt="Loading..."></img>
+                </div> :
+                    <Grid container spacing={3} justify="center">
+                        { exerciseList() }
+                    </Grid>
+                }
+            </div>
+        </div>
+)
+}
+
+export default Messages;
