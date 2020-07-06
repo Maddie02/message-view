d02b6251c1e3da1e49abdc68465a18281bbe1aa6
Madlen <madlensarkisian@gmail.com>
2020-07-06 15:14:03 +0300

    [New] Added CORS

--- server/src/main/java/com/server/controller/MessageController.java
+++ server/src/main/java/com/server/controller/MessageController.java
@@ -6,17 +6,17 @@ import com.server.model.Message;
 import com.server.repository.MessageRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
-
     @Autowired
     private MessageRepository messageRepository;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/getMessages")
     public List<Message> getMessages() {
         return messageRepository.findAll();
