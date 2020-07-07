cf1c7bd2280b191dc61e9fe5558ba47e8429911e
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-07 13:38:07 +0000

    Update HelloApplication.java with login methods

--- server/src/main/java/com/controller/HelloApplication.java
+++ server/src/main/java/com/controller/HelloApplication.java
@@ -2,16 +2,12 @@ package com.controller;
 
 import com.model.User;
 import com.repository.UserRepo;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletResponse;
-import java.security.Key;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -22,8 +18,7 @@ public class HelloApplication {
 	private UserRepo repository;
 
 	@GetMapping("/hello")
-	public String hello(
-			@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
@@ -33,57 +28,50 @@ public class HelloApplication {
 	}
 
 	@GetMapping(value = "/createUser")
-	public void createUser(
-			@RequestParam(value = "name", defaultValue = "Gosho") String name,
-			@RequestParam(value = "pass", defaultValue = "Tosho") String pass) {
+	public void createUser(@RequestParam(value = "name", defaultValue = "Gosho") String name, @RequestParam(value = "pass", defaultValue = "Tosho") String pass) {
 		repository.insert(new User(name, pass));
 	}
 
 	@RequestMapping(value="/register", method = RequestMethod.POST)
-	public void register(
-			@RequestParam("username") String username,
-			@RequestParam("password") String password,  HttpServletResponse response) {
-		repository.insert(new User(username, password));
-		response.setHeader("Location","http://localhost:3000/messages");
+	public void register(@RequestParam("username") String username, @RequestParam("password") String password,  HttpServletResponse response) {
+		repository.insert(new User(username, User.encryptPassword(password)));
+		response.setHeader("Location","http://localhost:3000");
 		response.setStatus(302);
 	}
 
-	@RequestMapping(value="/register2", method = RequestMethod.POST)
+	@RequestMapping(value="/register", method = RequestMethod.GET)
 	public void register(HttpServletResponse response) {
 		response.setHeader("Location","http://localhost:3000/sign-up");
 		response.setStatus(302);
 	}
-	@RequestMapping(value = "/login", method =  RequestMethod.GET)
-	public void loginUser(
-			@RequestParam(value = "username") String username,
-			@RequestParam(value = "password") String password, HttpServletResponse response){
-/*
-		User user = new User(username, password);
-		User user2 = repository.findByUsername(username);
 
-		if (user2 == null) {
+	@RequestMapping(value = "/login", method =  RequestMethod.POST)
+	public boolean login(
+			@RequestParam(value = "username") String username,
+			@RequestParam(value = "password") String password,
+			HttpServletResponse response)
+	{
+
+		User user = repository.findByUsername(username);
+		if (user == null) {
 			response.setHeader("Location","http://localhost:3000/sign-up");
 			response.setStatus(302);
-			return;
+			return false;
 		}
-
-		if (user.getPassword().equals(user2.getPassword())) {
-			//log user in
-			//start a session
-
-			response.setHeader("Location","http://localhost:3000/messages");
+		if(!user.validatePassword(password)){
+			response.setHeader("Location","http://localhost:3000/log-in");
 			response.setStatus(302);
-			return;
+			return false;
 		}
-
-
-		//System.out.println(encryptedPass);
-		response.setHeader("Location","http://localhost:3000/users");
-		response.setStatus(302);
-
-		 */
+		//session here
 		response.setHeader("Location","http://localhost:3000/messages");
 		response.setStatus(302);
+		return true;
 	}
 
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public void login(HttpServletResponse response) {
+		response.setHeader("Location","http://localhost:3000/sign-up");
+		response.setStatus(302);
+	}
 }
