5ec6c62bdca71387ea5007db3cbe623f1da9eba5
Madlen <madlensarkisian@gmail.com>
2020-07-07 11:13:47 +0300

    [New] getMessageById method returns all the data of the selected message

--- server/src/main/java/com/controller/MessageController.java
+++ server/src/main/java/com/controller/MessageController.java
@@ -1,24 +1,39 @@
 package com.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.model.Message;
 import com.repository.MessageRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MessageController {
 
     @Autowired
     private MessageRepository messageRepository;
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/getMessages")
     public List<Message> getMessages() {
         return messageRepository.findAll();
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<Message> getMessageById(@PathVariable("id") String id) {
+        Optional<Message> messageData = messageRepository.findById(id);
+
+        if (messageData.isPresent()) {
+            return new ResponseEntity<>(messageData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
