70064a3b55b3bdb1f93ad8b4d47f44cafbf8f9c6
Madlen <madlensarkisian@gmail.com>
2020-07-08 18:19:35 +0300

    [New] Added starter component for the form

---
+++ client/src/components/AddMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddMessage = () => {
+    return (
+        <div>
+            <br />
+            <h1>Form here</h1>
+        </div>
+    )
+}
+
+export default AddMessage;
