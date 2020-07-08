8866cfbc4e22f03d3a38598a76be6d68b077f443
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-08 11:58:14 +0000

    Update App.js with links for projects

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,8 @@ function App() {
           <Route path="/log-in" exact component={Login} />
           <Route path="/sign-up" exact component={SignUp} />
           <Route path='/users' component={User} />
+          <Route path='/projects' exact component={Project} />
+          <Route path='/projects/Alfabet' exact component={CurrentProject} />
           <Route path="/messages" exact component={Messages} />
           <Route path="/message/:id" exact component={MessageView} />
         </Switch>
