6755117118579094e8c8a5793a4fd92a10f688e7
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-09 08:44:59 +0000

    [Update] Progress in post method

--- server/src/main/java/com/controller/MessageController.java
+++ server/src/main/java/com/controller/MessageController.java
@@ -40,28 +40,37 @@ public class MessageController {
     }
 
     @RequestMapping(value="/createMessage", method = RequestMethod.POST)
-    public void createMessage(
+    public boolean createMessage(
             @RequestParam(value = "messageID") String messageID,
             @RequestParam(value = "text") String text,
             @RequestParam(value = "translation") boolean isForTranslation,
             @RequestParam(value = "documentation") boolean isForDocumentation,
             HttpServletResponse response)
     {
+        if(messageRepository.findByMessageID(messageID) != null)return false;
         EntityState state = EntityState.NEW;
         //User user = <User from Session> for creator and last modifier
         LocalDateTime date = LocalDateTime.now(); //for created Date and last modified Date
+
+        String consistentMessageId = "";
         /* TO DO: from where to take this parameters
-        String consistentMessageId = ?
         String consistentComponentId = ? - component
         String consistentProjectId = ? - project
         String version = ? -
         String messageType = ? - component
-        Map<String, EntityState> views
         */
 
+        Map<String, EntityState> views = new HashMap<String, EntityState>();
+
+
         //messageRepository.insert(new Message(consistentMessageId, consistentComponentId, consistentProjectId, messageID, text, version, messageType, state, isForDocumentation, isForTranslation, views, user.getUsername(), date, user.getUsername(), date)); //insert into collection
+        Message message = messageRepository.findByMessageID(messageID);
+        message.setConsistentMessageID(message.getId());
+        messageRepository.save(message);
+
 
         response.setHeader("Location","http://localhost:3000");
         response.setStatus(302);
+        return true;
     }
 }
