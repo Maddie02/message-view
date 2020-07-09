56f2b555ae724a9dd5a54e424ce596619b921982
Madlen <madlensarkisian@gmail.com>
2020-07-09 10:40:27 +0300

    Removed unnecessary imports

--- server/src/main/java/com/controller/ComponentController.java
+++ server/src/main/java/com/controller/ComponentController.java
@@ -1,21 +1,11 @@
 package com.controller;
 
 import com.model.Component;
-import com.model.Message;
-import com.model.User;
 import com.repository.ComponentRepository;
-import com.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
