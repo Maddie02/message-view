c4bdb636b6c8246dec71f2c1d8657daaf393c180
Gabriel Genkov <gabriel.genkov2002@gmail.com>
2020-07-09 08:40:28 +0000

    [Update] New method in MessageRepository

--- server/src/main/java/com/repository/MessageRepository.java
+++ server/src/main/java/com/repository/MessageRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends MongoRepository<Message, String> {
-
+    public Message findByMessageID(String messageID);
 }
